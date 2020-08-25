41a3f8c9ead5bebcfab488e7fc65d37edd7d97ab
KrishnaUpadhyay36 <kupadhyay977@gmail.com>
2020-08-25 15:18:17 +0530

    updated with Latest dependencies

--- src/test/java/CucumberTestRunner/TestRunner.java
+++ src/test/java/CucumberTestRunner/TestRunner.java
@@ -1,9 +1,9 @@
-
+package CucumberTestRunner;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
